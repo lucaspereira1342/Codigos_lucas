76a2534d042bdad9c807835649fe985787b5511a
lucaspereira1342 <lucaspereira@on.br>
2024-10-25 11:33:45 -0300

    Update Distribuição_de_temperaturas.py

--- Distribuição_de_temperaturas.py
+++ Distribuição_de_temperaturas.py
@@ -10,6 +10,10 @@ theta = np.array([0, 30, 45, 60, 90])
 
 def temperatura(l, angulo, i, a, b, c):
     angulo_rad = np.radians(angulo)
+    boltzmann = sci.k*1e07
+    planck = sci.Planck*1e07
+    velocidade_luz = sci.c*1e02
+    Ang = comprimento*1e-08
     temperatura = ((sci.Planck*sci.c)/(l*sci.k))*(1/(np.log(((2*sci.Planck*sci.c*i)/(l*(a + b*np.cos(angulo_rad)+2*c*np.cos(angulo_rad)**2)))+1)))
     return temperatura
 
